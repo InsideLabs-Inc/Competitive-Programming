946f19dff61dd11818e63b579734a80b65a9442f
arjun-ingole <arjuningole38@gmail.com>
2022-09-30 19:16:20 +0530

    Leetcode: Add solution to problem search-a-2d-matrix
    Signed-off-by: arjun-ingole <arjuningole38@gmail.com>

---
+++ LeetCode/SEARCH-A-2D-MATRIX.cpp
@@ -0,0 +1,22 @@
+// Link to Leetcode problem: https://leetcode.com/problems/search-a-2d-matrix/
+// Runtime: 3ms
+// Memory: 9.4mb
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]>target){
+                    return false;
+                } 
+                else if (matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
